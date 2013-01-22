339ad599bb5338c3b10bbb84c8ff0d58c3edb4dd
adil <mansari@buffalo.edu>
2013-01-22 11:56:49 -0500

    Initializing the app interface

---
+++ src/edu/buffalo/cse/cse486586/simplemessenger/MainActivity.java
@@ -0,0 +1,25 @@
+package edu.buffalo.cse.cse486586.simplemessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void sendMessage(View view) {
+    	//send message code here linked to the send button
+    }
+}
