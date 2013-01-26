107e0a17b5335c50de86beabc9c2da59f6814295
adil <mansari@buffalo.edu>
2013-01-26 13:55:29 -0500

    Application up and working, some cosmetic changes yet to be done

--- src/edu/buffalo/cse/cse486586/simplemessenger/MainActivity.java
+++ src/edu/buffalo/cse/cse486586/simplemessenger/MainActivity.java
@@ -3,10 +3,10 @@ package edu.buffalo.cse.cse486586.simplemessenger;
 import java.net.*;
 import java.io.*;
 import java.net.Socket;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.telephony.TelephonyManager;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -18,9 +18,11 @@ import android.widget.*;
 
 public class MainActivity extends Activity {
 
+	private Handler uiHandle= new Handler();
 	static final String TAG= "Adil";
 	static final String ERRORTAG= "Adil error";
-	int recvPort, sendPort;
+	int recvPort= 10000;
+	int sendPort;
 	String ipAddr= "10.0.2.2";
     
 	@Override
@@ -36,16 +38,14 @@ public class MainActivity extends Activity {
 		    	
 				if(portStr.equals("5554")) {
 					sendPort= 11112;
-					recvPort= 10000;
 				}
 				else if(portStr.equals("5556")) {
 					sendPort= 11108;
-					recvPort= 10000;
 				}
 				else
 					Log.v(TAG, "AVD portStr is neither 5554 nor 5556");
 				
-				Log.v(TAG, "recvPort"+Integer.toString(recvPort));
+				Log.v(TAG, "recvPort: "+Integer.toString(recvPort)+" sendPort: "+Integer.toString(sendPort));
 			}        	
         }).start();
         
@@ -62,36 +62,40 @@ public class MainActivity extends Activity {
         		ServerSocket servSocket= null;
         		try {
 					servSocket= new ServerSocket(recvPort);
+					Log.v(TAG, "Server Socket port: "+Integer.toString(servSocket.getLocalPort()));
 				} catch (IOException e) {
+					Log.v(ERRORTAG, ""+e.getMessage());
 					e.printStackTrace();
 				}
         		
         		while(true) {
         			try {
         				sock1= servSocket.accept();
+        				Log.v(TAG, "sock1: "+sock1.getInetAddress().getHostAddress()+sock1.isConnected()+Integer.toString(sock1.getLocalPort())+Integer.toString(sock1.getPort()));
         				//din = new DataInputStream(sock1.getInputStream());
         				BufferedReader br= new BufferedReader(new InputStreamReader(sock1.getInputStream()));
         				String str= br.readLine();
-        				updateTextView("recvd: " + str);
+        				Log.v(TAG, "recvd msg: "+str);
+        				updateTextView("recvd: "+str);
         				} 
         			
         			catch (IOException e) {
+        				Log.v(ERRORTAG, ""+e.getMessage());
         				e.printStackTrace();
-        				Log.e(ERRORTAG, ""+e.getMessage());
         			}
         			finally {
         				if (din!= null)
         					try {
         						din.close();
         					} catch (IOException e) {
-        						Log.e(ERRORTAG, ""+e.getMessage());
+        						Log.v(ERRORTAG, ""+e.getMessage());
         						e.printStackTrace();
         					}
         				if(sock1!=null)
         					try {
         						sock1.close();
         					} catch (IOException e) {
-        						Log.e(ERRORTAG, ""+e.getMessage());
+        						Log.v(ERRORTAG, ""+e.getMessage());
         						e.printStackTrace();
         					}	
         			}
@@ -124,31 +128,29 @@ public class MainActivity extends Activity {
     public void sendMessage(View view) {
     	EditText et= (EditText) findViewById(R.id.editText1);
     	final String msg= et.getText().toString();
-    	updateTextView("sent: " + msg);
+    	//updateTextView("sent: " + msg);
     	
     	new Thread(new Runnable() {
     		public void run() {
     			Socket sock= null;
     			try {
     				sock= new Socket(ipAddr, sendPort);
+    				Log.v(TAG, "send sock: "+sock.getInetAddress().getHostAddress()+sock.isConnected()+Integer.toString(sock.getPort()));
     			} catch (UnknownHostException e) {
-    				Log.e(ERRORTAG, ""+e.getMessage());
+    				Log.v(ERRORTAG, ""+e.getMessage());
     				e.printStackTrace();
     			} catch (IOException e) {
-    				Log.e(ERRORTAG, ""+e.getMessage());
+    				Log.v(ERRORTAG, ""+e.getMessage());
     				e.printStackTrace();
     			}
     			
     			try {
-    				//updateTextView("sent: " + msg);
-    				//DataOutputStream dout= new DataOutputStream(sock.getOutputStream());
     				PrintWriter out= new PrintWriter(sock.getOutputStream(),true);
-    				//dout.writeUTF(msg);
-    				//dout.flush();
     				out.println(msg);
+    				updateTextView("sent: " + msg);
     				}
     				catch (IOException e) {
-    					Log.e(ERRORTAG, ""+e.getMessage());
+    					Log.v(ERRORTAG, ""+e.getMessage());
     					e.printStackTrace();
     				}
     		}
@@ -167,10 +169,17 @@ public class MainActivity extends Activity {
     	return portStr;
     }
     
-    public void updateTextView(String msg) {
-    	TextView textView = (TextView)findViewById(R.id.textView1);
-    	textView.append(msg);
-    	textView.setMovementMethod(ScrollingMovementMethod.getInstance());
+    public void updateTextView(String message) {
+    	final String msg= message;
+    	uiHandle.post(new Runnable() {
+    		public void run() {
+    			TextView textView = (TextView)findViewById(R.id.textView1);
+    	    	Log.v(TAG, "updating textview");
+    	    	textView.append(msg);
+    	    	Log.v(TAG, "updated textview");
+    	    	textView.setMovementMethod(ScrollingMovementMethod.getInstance());
+    		}
+    	});
     }
 }
 
